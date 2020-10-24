1f79007097ec0495a545059ce4b59928a81e76ff
matsudan <daaamatsun@gmail.com>
2020-10-25 03:07:28 +0900

    Add get bank accounts

--- example.go
+++ example.go
@@ -83,7 +83,7 @@ func main() {
 	//	fmt.Println(err)
 	//}
 	//
-	//fmt.Printf("addresses: %#v\n",outputAddresses)
+	//fmt.Printf("addresses: %#v\n",outputAddresses.Addresses[0])
 
 	//outputCoinIns, err := client.GetCoinInList(ctx)
 	//if err != nil {
@@ -98,4 +98,11 @@ func main() {
 	//}
 	//
 	//fmt.Printf("coinouts: %#v\n",outputCoinOuts.CoinOuts)
+
+	outputBankAccounts, err := client.GetBankAccountList(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("bank accounts: %#v\n", outputBankAccounts.BankAccounts[0])
 }
