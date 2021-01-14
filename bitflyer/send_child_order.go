19a5f815010669e5621b78155f3910cf5a681393
matsudan <daaamatsun@gmail.com>
2021-01-14 23:54:56 +0900

    Add order request struct

---
+++ bitflyer/send_child_order.go
@@ -0,0 +1,13 @@
+package bitflyer
+
+import "github.com/matsudan/gobitflyer/bitflyer/types"
+
+type ChildOrderRequest struct {
+	ProductCode    types.ProductCode    `json:"product_code"`
+	ChildOrderType types.ChildOrderType `json:"child_order_type"`
+	Side           types.Side           `json:"side"`
+	Price          int64                `json:"price"`
+	Size           float64              `json:"size"`
+	MinuteToExpire int64                `json:"minute_to_expire"`
+	TimeInForce    types.TimeInForce    `json:"time_in_force"`
+}
